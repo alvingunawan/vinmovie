1e722d7a57c99e56e833d4a4acd751f6d3d2d12d
Alvin Gunawan <starvino70@gmail.com>
2024-04-05 10:01:34 +0700

    Commit pertama, Vin Movie versi 1

---
+++ js/script.js
@@ -0,0 +1,99 @@
+const searchButton = document.querySelector(".search-button");
+searchButton.addEventListener("click", async function () {
+  try {
+    const inputKeyword = document.querySelector(".input-keyword");
+    const movies = await getMovies(inputKeyword.value);
+    updateUI(movies);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+function editSearchContainer() {
+  const editableDiv = document.querySelector(".editable-div");
+  editableDiv.style.minHeight = "0";
+}
+
+function getMovies(keyword) {
+  return fetch("http://www.omdbapi.com/?apikey=d736285d&s=" + keyword)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((response) => {
+      if (response.response === false) {
+        throw new Error(response.error);
+      }
+      return response.Search;
+    });
+}
+
+function updateUI(movies) {
+  let cards = "";
+  movies.forEach((m) => (cards += showCards(m)));
+  const movieContainer = document.querySelector(".movie-container");
+  editSearchContainer();
+  movieContainer.innerHTML = cards;
+}
+
+// Event Binding
+document.addEventListener("click", async function (e) {
+  if (e.target.classList.contains("modal-detail-button")) {
+    const imdbid = e.target.dataset.imdbid;
+    const movieDetail = await getMovieDetail(imdbid);
+    updateUIDetail(movieDetail);
+  }
+});
+
+function getMovieDetail(imdbid) {
+  return fetch("http://www.omdbapi.com/?apikey=d736285d&i=" + imdbid)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(Error.statusText);
+      }
+      return response.json();
+    })
+    .then((m) => m);
+}
+
+function updateUIDetail(m) {
+  const movieDetail = showMovieDetail(m);
+  const modalBody = document.querySelector(".modal-body");
+  modalBody.innerHTML = movieDetail;
+}
+
+function showCards(m) {
+  return `<div class="col-md-4 my-3">
+                <div class="card" style="width:22rem;">
+                <img src="${m.Poster}" class="card-img-top" />
+                <div class="card-body">
+                    <h5 class="card-title">${m.Title}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">${m.Year}</h6>
+                    <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#movieDetailModal" data-imdbid="${m.imdbID}">Show Details</a>
+                </div>
+                </div>
+        </div>`;
+}
+
+function showMovieDetail(m) {
+  return `<div class="container-fluid">
+      <div class="row">
+        <div class="col-md-3">
+          <img src="${m.Poster}" class="img-fluid" />
+        </div>
+        <div class="col-md">
+          <ul class="list-group">
+            <li class="list-group-item"><h4>${m.Title}</h4></li>
+            <li class="list-group-item"><strong>Year : </strong>${m.Year}</li>
+            <li class="list-group-item"><strong>Director : </strong>${m.Director}</li>
+            <li class="list-group-item"><strong>Actors : </strong>${m.Actors}</li>
+            <li class="list-group-item"><strong>Writer : </strong>${m.Writer}</li>
+            <li class="list-group-item"><strong>Plot : </strong> <br />${m.Plot}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    `;
+}
